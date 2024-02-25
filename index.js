c19e42f21d284123755d4f7775f70689c0afe3ef
Henry Navas <navashenrym@gmail.com>
2024-02-25 01:31:16 -0300

    Dinamic content

---
+++ index.js
@@ -0,0 +1,19 @@
+import { createHighlights, createServices } from './src/components/hooks.js';
+
+const highlightsData = [
+    { title: "Responsive Design", content: "I specialize in creating responsive designs that look amazing on any device, ensuring a seamless user experience across platforms." },
+    { title: "React.js Expertise", content: "As a React.js enthusiast, I build dynamic and interactive user interfaces that engage your audience and drive results." },
+    {title: "Bootstrap Mastery", content: "With a solid command of Bootstrap, I leverage its powerful grid system and components to deliver polished and professional websites."},
+    {title: "Tailwind CSS Wizardry", content: "I harness the flexibility of Tailwind CSS to create custom, utility-first designs that align perfectly with your brand identity."}
+]
+
+const servicesData = [
+    {title: "Custom Web Development", content: "Elevate your online presence with bespoke web solutions tailored to your unique requirements. Let's turn your vision into reality.", button: "Get Started", image: "https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {title: "UI/UX Design", content: "Craft intuitive and visually stunning user interfaces that captivate your audience and drive engagement. Let's create experiences that leave a lasting impression.", button: "See Designs", image: "https://images.pexels.com/photos/196645/pexels-photo-196645.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {title: "Performance Optimization", content: "Optimize your website for speed and performance to enhance user satisfaction and boost conversion rates. Let's supercharge your digital presence.", button: "Optimize Now", image: "https://images.pexels.com/photos/730134/pexels-photo-730134.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+]
+
+document.addEventListener("DOMContentLoaded", 
+    createHighlights(highlightsData),
+    createServices(servicesData)
+);
---
+++ src/components/hooks.js
@@ -0,0 +1,52 @@
+function createHighlights (data) {
+  //Selecciono el container
+  const highlightsContainer = document.getElementById("internal-links-section");
+
+  // Itero sobre los datos y añado clases bootstrap
+  data.forEach(card => {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("internal-links-card", "col-md-3", "col-sm-6", "col-xsm-12");
+    
+  // Creo el contenido de la card
+        cardElement.innerHTML = `
+        <div>
+            <h4 class="normal-title">${card.title}</h4>
+            <p class="normal-text">${card.content}</p>
+        </div>
+        <div class="d-flex justify-content-between">
+            <button class="icon-button"><svg class="with-icon_icon__MHUeb" data-testid="geist-icon" fill="none" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" style="color:var(--geist-foreground);width:24px;height:24px"><circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 015.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/></svg></button>
+            <button class="icon-button"><svg class="with-icon_icon__MHUeb" data-testid="geist-icon" fill="none" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" style="color:var(--geist-foreground);width:24px;height:24px"><path d="M5 12h14"/><path d="M12 5l7 7-7 7"/></svg></button>
+        </div>
+        `;
+    
+  // Agrego la card al contenedor
+        highlightsContainer.appendChild(cardElement);
+      })
+}
+
+
+function createServices (data) {
+  const servicesContainer = document.getElementById("services-section");
+  data.forEach(card => {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("services-section-card", "container");
+    
+        cardElement.innerHTML = `
+        <div class="row align-items-center m-0">
+          <div class="col-md-8 col-xsm-12 mb-2">
+              <h4 class="normal-title">${card.title}</h4>
+              <p class="normal-text">${card.content}</p>
+              <button class="white-button">${card.button}</button>
+          </div>
+          <div class="col-md-4 col-xsm-12">
+              <figure><img src="${card.image}" alt="service-img"></figure>
+          </div>
+        </div>
+        `;
+    
+        servicesContainer.appendChild(cardElement);
+      })
+}
+
+
+export { createHighlights, createServices };
